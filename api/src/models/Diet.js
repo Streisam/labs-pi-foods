111e2a97ed39b1ad5890aea637de8fe9a857db5e
Samuel <samuel.esba@hotmail.com>
2022-12-19 23:11:02 -0400

    Models for the db created

---
+++ api/src/models/Diet.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('diet', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            unique: true
+        },
+        name: {
+            type: DataTypes.STRING
+        }
+    }, {
+        timestamps: false
+    });
+}
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,25 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    steps: {
+      type: DataTypes.ARRAY(DataTypes.STRING)
+    }
   });
 };
